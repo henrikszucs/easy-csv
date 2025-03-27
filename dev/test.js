98c55dbad4ae4b01c7e97bdbacb618efe22f1726
Henrik Szucs <henrikszucs6@gmail.com>
2025-03-27 17:19:13 +0200

    close to final release

--- dev/test.js
+++ dev/test.js
@@ -4,7 +4,99 @@ import CSV from "/src/csv.js";
 
 //tests
 (async function() {
-    console.log(CSV);
+    const compatreArrays = function(arr1, arr2) {
+        return JSON.stringify(arr1) === JSON.stringify(arr2);
+    };
+    let res;
+    let resInvert;
+    let testString;
+    let testResult;
 
+    // Test simple CSV
+    console.log("Test 1...");
+    testString = "1,2,3,4,5\n1,2,3,4,5";
+    testResult = [
+        ["1", "2", "3", "4", "5"],
+        ["1", "2", "3", "4", "5"]
+    ];
+    res = CSV.toCSV(testString, ",");
+    resInvert = CSV.toString(res, ",");
+    if (compatreArrays(res, testResult) === false || compatreArrays(resInvert, testString) === false) {
+        throw new Error("Unexpected");
+    }
+    console.log("Test 1... done");
+
+    // Test simple CSV multiple separators
+    console.log("Test 2...");
+    testString = "1,,2,,3,,4,,5\n1,,2,,3,,4,,5";
+    testResult = [
+        ["1", "", "2", "", "3", "", "4", "", "5"],
+        ["1", "", "2", "", "3", "", "4", "", "5"]
+    ];
+    res = CSV.toCSV(testString, ",");
+    resInvert = CSV.toString(res, ",");
+    if (compatreArrays(res, testResult) === false || compatreArrays(resInvert, testString) === false) {
+        throw new Error("Unexpected");
+    }
+    console.log("Test 2... done");
+
+    // Test simple CSV multicharacter separators
+    console.log("Test 3...");
+    testString = "1,,2,,3,,4,,5\n1,,2,,3,,4,,5";
+    testResult = [
+        ["1", "2", "3", "4", "5"],
+        ["1", "2", "3", "4", "5"]
+    ];
+    res = CSV.toCSV(testString, ",,");
+    resInvert = CSV.toString(res, ",,");
+    if (compatreArrays(res, testResult) === false || compatreArrays(resInvert, testString) === false) {
+        throw new Error("Unexpected");
+    }
+    console.log("Test 3... done");
+
+
+    // Test non rectangular CSV
+    console.log("Test 4...");
+    testString = "1,2,3,4\n1,2,3,4,5";
+    testResult = [
+        ["1", "2", "3", "4"],
+        ["1", "2", "3", "4", "5"]
+    ];
+    res = CSV.toCSV(testString, ",");
+    resInvert = CSV.toString(res, ",");
+    if (compatreArrays(res, testResult) === false || compatreArrays(resInvert, testString) === false) {
+        throw new Error("Unexpected");
+    }
+    console.log("Test 4... done");
+
+
+    // Test escaped CSV
+    console.log("Test 5...");
+    testString = `1,"test->""<-quote"\n2,"test->""""<-doublequote"`;
+    testResult = [
+        ["1", "test->\"<-quote"],
+        ["2", "test->\"\"<-doublequote"],
+    ];
+    res = CSV.toCSV(testString, ",");
+    resInvert = CSV.toString(res, ",");
+    console.log(resInvert);
+    if (compatreArrays(res, testResult) === false || compatreArrays(resInvert, testString) === false) {
+        throw new Error("Unexpected");
+    }
+    console.log("Test 5... done");
+
+    // Test quoted CSV
+    console.log("Test 6...");
+    testString = `1,"test->,<-quote"\n2,"test->,,<-doublequote"`;
+    testResult = [
+        ["1", "test->,<-quote"],
+        ["2", "test->,,<-doublequote"],
+    ];
+    res = CSV.toCSV(testString, ",");
+    resInvert = CSV.toString(res, ",");
+    if (compatreArrays(res, testResult) === false || compatreArrays(resInvert, testString) === false) {
+        throw new Error("Unexpected");
+    }
+    console.log("Test 6... done");
 
 })();
--- src/csv.js
+++ src/csv.js
@@ -1,7 +1,14 @@
+"use strict";
 
-const LoadCSV = function(str) {
+const regExpEscape = function (str) {
+    return str.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+const toCSV = function(str, sep=",") {
+    const sepRegExp = regExpEscape(sep);
     const result = [];
-    const pattern = /(\,|\r?\n|\r|^)(?:"([^"]*(?:""[^"]*)*)"|([^\,\r\n]*))/gi;
+    //const pattern = /(\,|\r?\n|\r|^)(?:"([^"]*(?:""[^"]*)*)"|([^\,\r\n]*))/gi;
+    const pattern = new RegExp("(" + sepRegExp + "|\\r?\\n|\\r|^)(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|([^" + sepRegExp + "\\r\\n]*))", "gi");
 
     //put end newline
     if (str[str.length - 1] !== "\n") {
@@ -10,10 +17,11 @@ const LoadCSV = function(str) {
 
     //parsing data
     let row = [];
+    let matches;
     while (matches = pattern.exec(str)) {
         //console.log(matches);
         //check the end of cell (newline)
-        if (matches[1] !== "," && matches[1] !== "") {
+        if (matches[1] !== sep && matches[1] !== "") {
         	//new line
             result.push(row);
             row = [];
@@ -30,6 +38,29 @@ const LoadCSV = function(str) {
     return result;
 };
 
+const toString = function(arr, sep=",") {
+    let str = "";
+    for (let i = 0; i < arr.length; i++) {
+        const row = arr[i];
+        for (let j = 0; j < row.length; j++) {
+            const cell = row[j];
+            if (cell.includes(sep) || cell.includes("\"")) {
+                str += "\"" + cell.replaceAll("\"", "\"\"") + "\"";
+            } else {
+                str += cell;
+            }
+            if (j < row.length - 1) {
+                str += sep;
+            }
+        }
+        if (i < arr.length - 1) {
+            str += "\n";
+        }
+    }
+    return str;
+};
+
 export default {
-	LoadCSV
+	toCSV,
+    toString
 };
